7f25715f24e2ec37bc96efd3698ba314f0283d3d
chinsheer <chinsheer05043@gmail.com>
2026-02-27 17:24:27 +0900

    Update scene(it's gone somehow)

---
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody2D;
+    private IMoveInputSource _enemyInput;
+
+    private bool _isGrounded = false;
+    [SerializeField] public float _moveSpeed = 3f;
+    [SerializeField] public float _jumpForce = 4f;
+
+    public float MoveSpeed { get => _moveSpeed; }
+    public float JumpForce { get => _jumpForce; }
+
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _enemyInput = GetComponent<IMoveInputSource>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 moveDirection = _enemyInput.MoveDirection;
+        _rigidbody2D.velocity = new Vector2(moveDirection.x * MoveSpeed, _rigidbody2D.velocity.y);
+
+        if (_enemyInput.Jump && _isGrounded)
+        {
+            _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, JumpForce);
+        }
+    }
+}
